e70b1184c4135bff436a44986df9e9e335fa4dc0
Camilo Andrés Morán López <camiloandresmoran@gmail.com>
2023-12-14 09:19:49 -0500

    Se quita los controles del dialogo generico

--- app/src/app/components/models/dialog.ts
+++ app/src/app/components/models/dialog.ts
@@ -3,13 +3,11 @@ export class Dialog {
     title: string;
 
     content: any;
-    control: any;
 
     constructor() {
         this.title = "";
 
         this.content = null;
-        this.control = null;
     }
 }
 
